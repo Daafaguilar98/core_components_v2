97d133cc8f268c557b0a21fe98f56d4714a92d0c
Diego Aguilar <daafaguilar98@gmail.com>
2019-04-17 15:30:06 -0500

    ui_components

---
+++ src/design/ui_components/searcher.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="searcher">
+    <el-input
+      placeholder="Buscar producto"
+      v-model="search"
+      class="input-with-select"
+      @keydown.enter.prevent="sendSearch"
+    >
+      <el-button slot="append" icon="el-icon-search"></el-button>
+    </el-input>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "koSearcher",
+  data() {
+    return {
+      search: ""
+    };
+  },
+  watch: {
+    search(value) {
+      if (value.length) {
+        this.$store.dispatch("products/FILTER_BY", {
+          type: "search",
+          data: value
+        });
+      } else {
+        this.$store.dispatch("products/FILTER_BY", { type: "all", data: "" });
+      }
+      // this.$emit("searcher", value)
+    }
+  }
+};
+</script>
+
+<style>
+.searcher {
+  padding: 5px;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+}
+</style>
